e10e40301b0bb7ba74ebce8fa42799af35d32a2e
Sugimoto takumi <c0a25132ea@edu.teu.ac.jp>
2026-06-16 14:44:25 +0900

    練習問題3実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,18 @@ DELTA = {
     pg.K_RIGHT: (+5, 0),
 }
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数 : こうかとんRect or 爆弾Rect
+    戻り値 : 判定結果タプル(横方向判定結果, 縦方向判定結果)
+    True : 画面内 / False : 画面外
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -50,10 +62,17 @@ def main():
                   sum_mv[0] += dx
                   sum_mv[1] += dy
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
         
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
 
         pg.display.update()
