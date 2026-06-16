c070218d4d37af9550633629906bd91a0dfc023a
Sugimoto takumi <c0a25132ea@edu.teu.ac.jp>
2026-06-16 14:24:08 +0900

    練習問題2実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -16,17 +17,31 @@ DELTA = {
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    
+    bg_img = pg.image.load("fig/pg_bg.jpg")   
+
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bb_img.set_colorkey((0, 0, 0))
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
+
+    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -36,6 +51,11 @@ def main():
                   sum_mv[1] += dy
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
